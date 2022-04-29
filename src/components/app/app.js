951a21acb6907630ef93bb495cd3da03e676a848
ValentinaGorlan <valentina.gorlanova.ru@gmail.com>
2022-04-29 03:09:21 +0300

    add delete method

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,46 @@
+import { Component } from "react";
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
-import "./app.css";
 import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-function App() {
+import "./app.css";
 
-  const data = [
-    { id: 1, name: "John Smith", salary: 900, increase: false},
-    { id: 2, name: "Jack Daniels", salary: 3000, increase: true },
-    { id: 3, name: "James Bond", salary: 500, increase: false },
-  ];
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data : [
+        { id: 1, name: "John Smith", salary: 900, increase: false},
+        { id: 2, name: "Jack Daniels", salary: 3000, increase: true },
+        { id: 3, name: "James Bond", salary: 500, increase: false },
+      ]
+    }
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+        </div>
+        <EmployeesList data={this.state.data}
+        onDelete={this.deleteItem} />
+        <EmployeesAddForm />
       </div>
-      <EmployeesList data={data} />
-      <EmployeesAddForm />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -22,7 +22,7 @@ class EmployeesListItem extends Component{
   }
 
   render () {
-    const {name, salary} = this.props;
+    const {name, salary, onDelete} = this.props;
     const {increase, like} = this.state;
     let classNames = "list-group-item d-flex justify-content-between";
     if (increase)  {
@@ -50,7 +50,9 @@ class EmployeesListItem extends Component{
           onClick={this.onIncrease}>
             <i className="fas fa-cookie"></i>
           </button>
-          <button type="button" className="btn-trash btn-sm">
+          <button type="button" 
+          className="btn-trash btn-sm"
+          onClick={onDelete}>
             <i className="fas fa-trash"></i>
           </button>
           <i className="fas fa-star"></i>
